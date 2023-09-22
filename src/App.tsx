389ae925272187a6fdc1b2ed76508af8d269d9fc
magnogois <magnogois@MacBook-Pro-de-magnogois.local>
2023-09-22 00:49:02 -0300

    feat: insert eslint in project

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import "./App.css";
+import './App.css';
 
-import { useState } from "react";
+import { useState } from 'react';
 
-import viteLogo from "/vite.svg";
+import viteLogo from '/vite.svg';
 
-import reactLogo from "./assets/react.svg";
+import reactLogo from './assets/react.svg';
 
 function App() {
   const [count, setCount] = useState(0);
