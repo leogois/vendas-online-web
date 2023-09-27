43d146eb0c6bcc62ed7778b94d83871eaecaf019
magnogois <magnogois@MacBook-Pro-de-magnogois.local>
2023-09-27 20:17:32 -0300

    feat: insert token in localStorage

--- src/shared/hooks/useRequests.ts
+++ src/shared/hooks/useRequests.ts
@@ -23,15 +23,16 @@ export const useRequest = () => {
       });
   };
 
-  const postRequest = async (url: string, body: unknown) => {
+  const postRequest = async <T>(url: string, body: unknown): Promise<T | undefined> => {
     setLoading(true);
-    const returnData = await connectionAPIPost(url, body)
+    const returnData = await connectionAPIPost<T>(url, body)
       .then((result) => {
         setNotification('Entrando...', 'success');
         return result;
       })
       .catch((error: Error) => {
         setNotification(error.message, 'error');
+        return undefined;
       });
 
     setLoading(false);
